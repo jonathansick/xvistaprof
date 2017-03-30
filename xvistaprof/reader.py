b7cc9c5d2275a87849701e8a7307e26680bb740a
Jonathan Sick <jsick@lsst.org>
2017-03-30 07:44:17 -0700

    Update genfromtxt skip_header for numpy 1.10+

--- xvistaprof/reader.py
+++ xvistaprof/reader.py
@@ -15,7 +15,7 @@ def xvista_table_reader(filename):
           ('ELLMAG', np.float), ('ELLMAG_err', np.float), ('XC', np.float),
           ('YC', np.float), ('FRACONT', np.float), ('A1', np.float),
           ('A2', np.float), ('A4', np.float), ('CIRCMAG', np.float)]
-    data = np.genfromtxt(filename, dtype=np.dtype(dt), skiprows=15,
+    data = np.genfromtxt(filename, dtype=np.dtype(dt), skip_header=15,
                          missing_values='*', filling_values=np.nan)
     return Table(data)
 
